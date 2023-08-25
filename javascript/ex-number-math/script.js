50058877f35f317b5a31e19d76eafe22ad02c321
MuriloPolicastro <murilopolicastro@hotmail.com>
2023-08-25 10:11:23 -0300

    Update script.js

--- javascript/ex-number-math/script.js
+++ javascript/ex-number-math/script.js
@@ -4,6 +4,9 @@ const texto = document.getElementById('texto');
 
 numeroTitulo.innerHTML = numero;
 
-texto.innerHTML = `<p> A raiz quadrado dele é ${numero**1/2} </p>`
-texto.innerHTML += `<p> Arredondado para baixo: ${Math.floor(numero)} </p>`
-texto.innerHTML += `<p> Arredondado para cima: ${Math.ceil(numero)} </p>`
+texto.innerHTML = `<p> A raiz quadrado dele é ${numero**0.5}. </p>`
+texto.innerHTML += `<p> ${numero} é inteiro: ${Number.isInteger(numero)}. </p>`
+texto.innerHTML += `<p> ${numero} é NaN: ${Number.isNaN(numero)}. </p>`
+texto.innerHTML += `<p> Arredondado para baixo: ${Math.floor(numero)}. </p>`
+texto.innerHTML += `<p> Arredondado para cima: ${Math.ceil(numero)}. </p>`
+texto.innerHTML += `<p> Com duas casas decimais: ${numero.toFixed(2)}. </p>`
