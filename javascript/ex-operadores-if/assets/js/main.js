56c8e820cd6f7eba4fb57b24b1d49e3852a63af2
MuriloPolicastro <murilopolicastro@hotmail.com>
2023-08-29 10:56:56 -0300

    ex

--- javascript/ex-operadores-if/assets/js/main.js
+++ javascript/ex-operadores-if/assets/js/main.js
@@ -1,6 +1,5 @@
 const pesoForm = document.querySelector('#peso');
 const alturaForm = document.querySelector('#altura')
-const resultado = document.querySelector('.resultado');
 
 function enviar(imc){
     const peso = Number(pesoForm.value);
@@ -8,27 +7,39 @@ function enviar(imc){
     imc = peso / (altura**2);
     
     if (!pesoForm.value || !alturaForm.value) {
-        window.alert('Peso e/ou Altura digitados são inválidos')
-    } else if (pesoForm.value <= 0 || alturaForm.value <= 0) {
-        window.alert('Peso e/ou Altura devem ser diferentes de 0')
-    } else {
-        if (imc < 18.5) {
-            resultado.innerHTML = `Seu IMC é ${imc} (Abaixo do Peso)`;
-        } else if (imc > 18.5) {
-            resultado.innerHTML = `Seu IMC é ${imc} (Peso Normal)`;
-        } else if (imc >= 25) {
-            resultado.innerHTML = `Seu IMC é ${imc} (Sobrepeso)`;
-        } else if (imc >= 30) {
-            resultado.innerHTML = `Seu IMC é ${imc} (Obesidade Grau 1)`;
-        } else if (imc >= 35) {
-            resultado.innerHTML = `Seu IMC é ${imc} (Obesidade Grau 2)`;
-        } else if (imc >= 40) {
-            resultado.innerHTML = `Seu IMC é ${imc} (Obesidade Grau 3)`;
-        }
+        setResultado('Peso e/ou Altura digitados são inválidos', false);
+        return
     }
+
+    if (pesoForm.value <= 0 || alturaForm.value <= 0) {
+        setResultado('Peso e/ou Altura devem ser diferentes de 0', false);
+        return
+    }
+    if (imc >= 40) return setResultado (`Seu IMC é ${imc.toFixed(2)} (Obesidade Grau 3)`, true);
+    if (imc >= 35) return setResultado (`Seu IMC é ${imc.toFixed(2)} (Obesidade Grau 2)`, true);
+    if (imc >= 30) return setResultado (`Seu IMC é ${imc.toFixed(2)} (Obesidade Grau 1)`, true);
+    if (imc >= 25) return setResultado (`Seu IMC é ${imc.toFixed(2)} (Sobrepeso)`, true); 
+    if (imc > 18.5)return setResultado (`Seu IMC é ${imc.toFixed(2)} (Peso Normal)`, true); 
+    if (imc < 18.5) return setResultado(`Seu IMC é ${imc.toFixed(2)} (Abaixo do Peso)`, true);
 }
 
 function criaP () {
     const p = document.createElement('p');
     return p;
+}
+
+function setResultado (msg, isValid) {
+    const resultado = document.querySelector('.resultado');
+    resultado.innerHTML = '';
+
+    const p = criaP();
+
+    if (isValid) {
+        p.classList.add('g-resultado');
+    } else {
+        p.classList.add('bad');
+    }
+
+    p.innerHTML = msg;
+    resultado.appendChild(p);
 }
