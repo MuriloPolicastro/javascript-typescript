8a4cbb4cee7c01c2c693cadc4aac402a11d9cceb
MuriloPolicastro <murilopolicastro@hotmail.com>
2023-08-31 17:11:43 -0300

    Update script.js

--- javascript/ex-lgc-01/script.js
+++ javascript/ex-lgc-01/script.js
@@ -1,11 +1,17 @@
 //Escreva uma função que recebe 2 números e retorne o maior deles.
-
+/*
 function maiorNum(n1, n2) {
-    if (n1 > n2) {
-        return n1;
-    } else {
-        return n2;
-    }
+    return n1 > n2 ? n1 : n2;
 }
+console.log(maiorNum(8,9))
+*/
 
-maiorNum(8,9);
+//Arrow Function
+/*const max = (x, y) => {
+    return x > y ? x : y;
+}*/
+
+//Abreviado:
+const max = (x, y) => {return x > y ? x : y};
+
+console.log(max(200, 30));
