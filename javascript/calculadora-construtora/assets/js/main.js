253e7b239d645f820e34725b18b5555338153ee2
MuriloPolicastro <murilopolicastro@hotmail.com>
2023-10-09 10:44:27 -0300

    calc-construct

---
+++ javascript/calculadora-construtora/assets/js/main.js
@@ -0,0 +1,79 @@
+function criaCalculadora() {
+
+    return {
+        display: document.querySelector('.display'),
+
+        inicia() {
+            this.pressBtn();
+            this.pressEnter();
+            this.pressBack();
+        },
+
+        pressEnter() {
+            this.display.addEventListener('keyup', e => {
+                if (e.keyCode === 13) {
+                    this.resolveConta();
+                };
+            });
+        },
+
+        pressBack() {
+            this.display.addEventListener('keydown', e => {
+                if (e.keyCode === 8) {
+                    e.preventDefault();
+                    this.clearDisplay();
+                };
+            });
+        },
+
+        pressBtn() {
+            document.addEventListener('click', e => {
+                const el = e.target;
+
+                if (el.classList.contains('btn-num')) {
+                    this.mostrarNoDisplay(el.innerText);
+                };
+                if (el.classList.contains('btn-clear')) {
+                    this.clearDisplay();
+                };
+                if (el.classList.contains('btn-eq')) {
+                    this.resolveConta();
+                };
+                if (el.classList.contains('btn-del')) {
+                    this.apagaUm();
+                };
+            });
+        },
+
+        mostrarNoDisplay(valor) {
+            this.display.value += valor;
+        },
+
+        clearDisplay() {
+            this.display.value = '';
+        },
+
+        apagaUm() {
+            this.display.value = this.display.value.slice(0, -1);
+        },
+
+        resolveConta() {
+            let conta = this.display.value;
+            try {
+                conta = eval(conta);
+
+                if (!conta){
+                    alert('Conta Inválida')
+                }
+
+                this.display.value = String(conta);
+            } catch(err) {
+                alert('Conta Inválida');
+                return;
+            }
+        },
+    };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
