3eb91bc06d2d273e8e9b912b8633cf385a9f5d4b
MuriloPolicastro <murilopolicastro@hotmail.com>
2024-01-17 12:47:24 -0300

    estudos

--- javascript/01-estudos/05-array-avancado/85-map.js
+++ javascript/01-estudos/05-array-avancado/85-map.js
@@ -1,5 +1,6 @@
 // a Map irá receber um novo array do mesmo tamanho do original, porém com valores alterados sem modificar o original, apenas usando como base.
 // Funciona da mesma maneiro do Filter, recebe uma função de callback, cria um novo array.
+// Map é um metodo que se baseia no obj original logo ela pode altera-ló.
 
 const numeros = [5, 80, 60, 70, 8, 9, 2, 1, 98, 13, 22];
 const numerosDobrados = numeros.map(valor => valor * 2);
@@ -18,3 +19,18 @@ const pessoas = [
     { nome: 'Murilo', idade: 22 },
     { nome: 'Nicole', idade: 20 },
 ];
+const nomes = pessoas.map(obj => obj.nome);
+const idades = pessoas.map(obj => ({ idade: obj.idade }));
+// ou poderia usar (delete obj.nome).
+const comIds = pessoas.map(function(obj, indice) {
+/*
+Desta maneira a função ira iterar encima do objeto original e assim ira altera-ló
+    obj.id = indice + 1;
+    return obj;
+*/
+// Desta maneira não ira alterar o objeto original
+    const newObj = {...obj};
+    newObj.id = indice + 1;
+    return newObj;
+});
+console.log(comIds);
---
+++ javascript/01-estudos/05-array-avancado/86-reduce.js
@@ -0,0 +1,22 @@
+// Reduce consegue fazer o que Filter e Map fazem, porém não é aconselhavel, use as função respectivas para cada coisa.
+// Reduce reduz um array em um unico elemento.
+// Na função de callback do Reduce teremos um novo parametro, o 'acumulador'.
+// Pode ser inserido logo depois da função no metodo Reduce um valor para o acumulador.
+
+
+// Some todos os numeros (Reduce).
+// Retorne um array com os pares (Filter).
+// Retorne um array com o dobro dos valores (Map).
+const numeros = [5, 80, 60, 70, 8, 9, 2, 1, 98, 13, 22];
+const total = numeros.reduce(((acumulador, valor) => acumulador += valor), 0);
+// Essa função abaixa deve ser feito usando o Filter.
+const arrayPares = numeros.reduce(function(acumulador, valor) {
+    if (valor % 2 === 0) acumulador.push(valor);
+    return acumulador;
+}, []);
+// Essa função abaixa deve ser feito usando o Map.
+const arrayDobro = numeros.reduce(function(acumulador, valor) {
+    acumulador.push(valor * 2);
+    return acumulador;
+}, []);
+console.log(arrayDobro);
