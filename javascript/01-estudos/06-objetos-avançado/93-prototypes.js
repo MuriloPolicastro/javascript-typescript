b9f30948e3ebf1128a81a7295468dca9f777d8f3
MuriloPolicastro <murilopolicastro@hotmail.com>
2024-01-19 17:00:16 -0300

    Create 93-prototypes.js

---
+++ javascript/01-estudos/06-objetos-avançado/93-prototypes.js
@@ -0,0 +1,6 @@
+// Prototype são prototipos criados externo a uma função construtora para que todos os objetos criados a partir dela possam usar sem que seja necessario criar um novo metodo dentro de cada objeto.
+
+// Todos os objetos são linkados a esse prototypes.
+
+// Ao inves de criar um metodo igual dentro de todo objeto que for criado, criamos um prototype externo e todos usaram o mesmo tendo assim um aumento de performace.
+
