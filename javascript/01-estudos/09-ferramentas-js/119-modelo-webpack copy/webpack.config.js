215f5f5366711a7884c8c3d0428016dc94b4a504
MuriloPolicastro <murilopolicastro@hotmail.com>
2024-04-16 21:48:09 -0300

    estd

---
+++ javascript/01-estudos/09-ferramentas-js/119-modelo-webpack copy/webpack.config.js
@@ -0,0 +1,37 @@
+// o Node.js usa o sistema de modulos CommonJS(padrao Node), diferente do JS ES6(padrao Javascript).
+// Todo arquivo é um modulo diferente para o Node.
+
+const path = require('path');
+
+// o objeto abaixo tem o mesmo objetivo do 'return' em funções normais, a resposta será apenas o que está dentro dela.
+// Esse objeto contem a config do webpack.
+
+module.exports = {
+    mode: 'production',
+    entry: './src/main.js',
+    output: {
+       path: path.resolve(__dirname, 'public', 'assets', 'js'),
+       filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            // Para o webpack não ficar analisando essa pasta.
+            exclude: /node_modules/,
+            // Qual vai ser a extensão dos arquivos que vão ser testados.
+            test: /\.js$/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/env']
+                }
+            }
+        }, {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+            }]
+    },
+    devtool: 'source-map'
+};
+// O resolve procura o diretorio sozinho com base no nome das pastas inseridos.
+// __dirname é o diretorio atual.
+// O (devtool: 'souce-map') é usado para mapear o arquivo e saber de onde vem cada coisa.
